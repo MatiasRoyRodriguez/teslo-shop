fee8c1eccc956d1833eba5d765c84a1cfb9a11f8
Matías Roy Rodriguez <matiasrr980@gmail.com>
2023-02-21 19:25:02 -0300

    feat: add product

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,26 +1,49 @@
 import { Injectable } from '@nestjs/common';
+import { InternalServerErrorException } from '@nestjs/common/exceptions';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
+import { Product } from './entities/product.entity';
 
 @Injectable()
 export class ProductsService {
-  create(createProductDto: CreateProductDto) {
-    return 'This action adds a new product';
-  }
 
-  findAll() {
-    return `This action returns all products`;
-  }
+	constructor(
 
-  findOne(id: number) {
-    return `This action returns a #${id} product`;
-  }
+		@InjectRepository(Product)
+		private readonly productRepository: Repository<Product>
+	) { }
 
-  update(id: number, updateProductDto: UpdateProductDto) {
-    return `This action updates a #${id} product`;
-  }
+	async create(createProductDto: CreateProductDto) {
 
-  remove(id: number) {
-    return `This action removes a #${id} product`;
-  }
+		try {
+
+			const product = this.productRepository.create(createProductDto);
+			await this.productRepository.save(product);
+
+			return product;
+
+		} catch (error) {
+			console.log(error);
+			throw new InternalServerErrorException('Help!')
+		}
+
+	}
+
+	findAll() {
+		return `This action returns all products`;
+	}
+
+	findOne(id: number) {
+		return `This action returns a #${id} product`;
+	}
+
+	update(id: number, updateProductDto: UpdateProductDto) {
+		return `This action updates a #${id} product`;
+	}
+
+	remove(id: number) {
+		return `This action removes a #${id} product`;
+	}
 }
