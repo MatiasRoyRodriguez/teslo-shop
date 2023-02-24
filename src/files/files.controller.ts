41f161a8c29fb53cd51fed9798c426b29bb6fa01
Matías Roy Rodriguez <matiasrr980@gmail.com>
2023-02-24 20:55:16 -0300

    feat: save images in file system

--- src/files/files.controller.ts
+++ src/files/files.controller.ts
@@ -3,6 +3,7 @@ import { FileInterceptor } from '@nestjs/platform-express';
 import { FilesService } from './files.service';
 import { fileFilter } from './helpers/fileFilter.helper';
 import { BadRequestException } from '@nestjs/common/exceptions';
+import { diskStorage } from 'multer';
 
 
 @Controller('files')
@@ -11,11 +12,15 @@ export class FilesController {
 
 	@Post('product')
 	@UseInterceptors(FileInterceptor('file', {
-		fileFilter: fileFilter
+		fileFilter: fileFilter,
+		// limits: { fileSize: 1000 }
+		storage: diskStorage({
+			destination: './static/uploads'	
+		})
 	}))
 	uploadProductImage(@UploadedFile() file: Express.Multer.File) {
-		
-		if(!file){
+
+		if (!file) {
 			throw new BadRequestException('Make sure that file is an image');
 		}
 
