9c4d89d877e3cb04a33571b77c2f2c4cc559fc26
Matías Roy Rodriguez <matiasrr980@gmail.com>
2023-02-25 21:15:32 -0300

    feat: implement custom decorators

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,10 +1,13 @@
-import { Controller, Get, Post, Body, UseGuards, Req, Headers } from '@nestjs/common';
+import { Controller, Get, Post, Body, UseGuards, Req, Headers, SetMetadata } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { IncomingHttpHeaders } from 'http';
 import { AuthService } from './auth.service';
 import { GetUser, RawHeaders } from './decorators';
+import { RoleProtected } from './decorators/role-protected.decorator';
 import { CreateUserDto, LoginUserDto } from './dto';
 import { User } from './entities/user.entity';
+import { UserRoleGuard } from './guards/user-role.guard';
+import { ValidRoles } from './interfaces';
 
 @Controller('auth')
 export class AuthController {
@@ -40,4 +43,17 @@ export class AuthController {
 		}
 	}
 
+	// @SetMetadata('roles', ['admin','super-user'])
+	@Get('private2')
+	@RoleProtected( ValidRoles.superUser, ValidRoles.user )
+	@UseGuards(AuthGuard(), UserRoleGuard)
+	privateRoute2(
+		@GetUser() user: User
+	) {
+
+		return {
+			ok: true,
+			user
+		}
+	}
 }
