77e243facee94cdec0c9578f261d221f9a16991b
Matías Roy Rodriguez <matiasrr980@gmail.com>
2023-02-25 21:39:28 -0300

    fix: remove auth for seed

--- src/seed/seed.controller.ts
+++ src/seed/seed.controller.ts
@@ -1,7 +1,5 @@
 import { Controller, Get } from '@nestjs/common';
-import { Auth } from '../auth/decorators';
 import { SeedService } from './seed.service';
-import { ValidRoles } from '../auth/interfaces/valid-roles';
 
 
 @Controller('seed')
@@ -9,7 +7,7 @@ export class SeedController {
 	constructor(private readonly seedService: SeedService) { }
 
 	@Get()
-	@Auth(ValidRoles.admin)
+	// @Auth(ValidRoles.admin)
 	executeSeed() {
 		return this.seedService.runSeed();
 	}
