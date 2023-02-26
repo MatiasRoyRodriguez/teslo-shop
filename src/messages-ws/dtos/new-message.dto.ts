835eec9645379bb5aa917e521ed3c2d011a20220
Matías Roy Rodriguez <matiasrr980@gmail.com>
2023-02-26 11:55:06 -0300

    feat: listen from server side

---
+++ src/messages-ws/dtos/new-message.dto.ts
@@ -0,0 +1,9 @@
+import { IsString, MinLength } from "class-validator";
+
+export class NewMessageDTO {
+
+		@IsString()
+		@MinLength(1)
+		message: string;
+
+}
--- src/messages-ws/messages-ws.gateway.ts
+++ src/messages-ws/messages-ws.gateway.ts
@@ -1,5 +1,7 @@
 import { OnGatewayConnection, OnGatewayDisconnect, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
+import { SubscribeMessage } from '@nestjs/websockets/decorators';
 import { Socket, Server } from 'socket.io';
+import { NewMessageDTO } from './dtos/new-message.dto';
 import { MessagesWsService } from './messages-ws.service';
 @WebSocketGateway({ cors: true })
 export class MessagesWsGateway implements OnGatewayConnection, OnGatewayDisconnect {
@@ -18,4 +20,8 @@ export class MessagesWsGateway implements OnGatewayConnection, OnGatewayDisconne
 		this.wss.emit('clients-updated', this.messagesWsService.getConnectedClients());
 	}
 
+	@SubscribeMessage('message-from-client')
+	onMessageFromClient( client: Socket, payload: NewMessageDTO){
+			console.log(client.id, payload);
+	}
 }
